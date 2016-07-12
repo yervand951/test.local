a9f452a220c49d40da9f8d008107d3e6e19fc0bb
yervand951 <ervandghukasyan@gmail.com>
2016-07-12 11:07:44 +0400

    Add files via upload

--- confs/db.php
+++ confs/db.php
@@ -1,9 +1,25 @@
 <?php
-$servername = 'test.local';
-$username = '';
-$password = '';
-$db = 'testlogin';
+
+class DB{
+    private $servername = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $db = 'testlogin';
+    public $conf;
+
+    public function __construct()
+    {
+        $conf = new mysqli($this->servername,$this->username,$this->password,$this->db);
+        if ($conf->connect_error) {
+            die("Connection failed: " . $conf->connect_error);
+        }
+        $this->conf = $conf;
+    }
 
 
-//$conf = new mysqli($servername,$username,$password,$db);
+
+
+
+}
+
 ?>
